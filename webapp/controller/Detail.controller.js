a0fe77dc685bfce301dd8f01dc0a57a480f3efdb
penx1999@yahoo.com <penx1999@yahoo.com>
2026-03-12 22:53:26 -0600

    feat: Detail rediseñado con tabla dinámica desde DynamicFieldSet + DataSetAsoc

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -1,51 +1,134 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/core/routing/History",
-    "sap/m/MessageToast"
-], function (Controller, History, MessageToast) {
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
+    "sap/m/MessageBox",
+    "sap/m/Column",
+    "sap/m/Text",
+    "sap/m/ColumnListItem",
+    "sap/m/Label"
+], function (Controller, History, JSONModel, Filter, FilterOperator, MessageBox, Column, Text, ColumnListItem, Label) {
     "use strict";
 
     return Controller.extend("t_project1.controller.Detail", {
 
         onInit: function () {
+            var oModel = new JSONModel({
+                productAllocationObject: "",
+                columns: [],
+                rows: [],
+                busy: false
+            });
+            this.getView().setModel(oModel, "detailModel");
+
             var oRouter = this.getOwnerComponent().getRouter();
             oRouter.getRoute("RouteDetail").attachPatternMatched(this._onRouteMatched, this);
         },
 
-        _onRouteMatched: function () {
-            var oDetailModel = this.getOwnerComponent().getModel("detailModel");
-            if (oDetailModel) {
-                this.getView().setModel(oDetailModel, "detailModel");
-            }
+        _onRouteMatched: function (oEvent) {
+            var sQuotaId = decodeURIComponent(oEvent.getParameter("arguments").quotaId);
+            var oModel = this.getView().getModel("detailModel");
+            oModel.setProperty("/productAllocationObject", sQuotaId);
+            oModel.setProperty("/busy", true);
+            this._loadDynamicFields(sQuotaId);
+        },
+
+        _loadDynamicFields: function (sProductAllocationObject) {
+            var oODataModel = this.getOwnerComponent().getModel();
+            var oModel = this.getView().getModel("detailModel");
+            var oBundle = this.getView().getModel("i18n").getResourceBundle();
+            var that = this;
+
+            oODataModel.read("/DynamicFieldSet", {
+                filters: [new Filter("tablename", FilterOperator.EQ, sProductAllocationObject)],
+                urlParameters: { "$expand": "DataSetAsoc" },
+                success: function (oData) {
+                    var aFields = oData.results || [];
+                    aFields.sort(function (a, b) {
+                        return parseInt(a.position) - parseInt(b.position);
+                    });
+
+                    var aColumns = aFields.map(function (oField) {
+                        return {
+                            name: oField.name,
+                            label: oField.description || oField.name
+                        };
+                    });
+
+                    var mRows = {};
+                    aFields.forEach(function (oField) {
+                        var aData = (oField.DataSetAsoc && oField.DataSetAsoc.results) ? oField.DataSetAsoc.results : [];
+                        aData.forEach(function (oEntry) {
+                            var sUUID = oEntry.CHARCVALUECOMBINATIONUUID;
+                            if (!mRows[sUUID]) {
+                                mRows[sUUID] = {
+                                    CHARCVALUECOMBINATIONUUID: sUUID,
+                                    PRODALLOCPERDSTARTUTCDATETIME: oEntry.PRODALLOCPERDSTARTUTCDATETIME,
+                                    PRODALLOCPERIODENDUTCDATETIME: oEntry.PRODALLOCPERIODENDUTCDATETIME
+                                };
+                            }
+                            mRows[sUUID][oField.name] = oEntry.Value;
+                        });
+                    });
+
+                    var aRows = Object.values(mRows);
+
+                    oModel.setProperty("/columns", aColumns);
+                    oModel.setProperty("/rows", aRows);
+                    oModel.setProperty("/busy", false);
+
+                    that._buildTable(aColumns);
+                },
+                error: function (oError) {
+                    oModel.setProperty("/busy", false);
+                    var sMsg = oBundle.getText("msgReadError");
+                    try {
+                        var oResp = JSON.parse(oError.responseText);
+                        sMsg = oResp.error.message.value || sMsg;
+                    } catch (e) {}
+                    MessageBox.error(sMsg);
+                }
+            });
+        },
+
+        _buildTable: function (aColumns) {
+            var oTable = this.byId("idDynamicTable");
+            if (!oTable) { return; }
+
+            oTable.destroyColumns();
+
+            oTable.addColumn(new Column({ header: new Label({ text: "Período Inicio" }) }));
+            oTable.addColumn(new Column({ header: new Label({ text: "Período Fin" }) }));
+
+            aColumns.forEach(function (oCol) {
+                oTable.addColumn(new Column({ header: new Label({ text: oCol.label }) }));
+            });
+
+            var oTemplate = oTable.getBindingInfo("items") && oTable.getBindingInfo("items").template;
+            if (oTemplate) { oTemplate.destroy(); }
+
+            var oCells = [];
+            oCells.push(new Text({ text: "{detailModel>PRODALLOCPERDSTARTUTCDATETIME}" }));
+            oCells.push(new Text({ text: "{detailModel>PRODALLOCPERIODENDUTCDATETIME}" }));
+            aColumns.forEach(function (oCol) {
+                oCells.push(new Text({ text: "{detailModel>" + oCol.name + "}" }));
+            });
+
+            oTable.bindItems({
+                path: "detailModel>/rows",
+                template: new ColumnListItem({ cells: oCells })
+            });
         },
 
         onNavBack: function () {
             var oHistory = History.getInstance();
             var sPreviousHash = oHistory.getPreviousHash();
-
             if (sPreviousHash !== undefined) {
                 window.history.go(-1);
             } else {
                 this.getOwnerComponent().getRouter().navTo("RouteListReport", {}, true);
-            }
-        },
-
-        formatActivationStatus: function (sStatus) {
-            switch (sStatus) {
-                case "1": return "Success";
-                case "2": return "None";
-                case "3": return "Error";
-                default:  return "None";
-            }
-        },
-
-        formatDate: function (oDate) {
-            if (!oDate) { return ""; }
-            try {
-                var oDateFormat = sap.ui.core.format.DateFormat.getDateInstance({ pattern: "dd/MM/yyyy" });
-                return oDateFormat.format(new Date(oDate));
-            } catch (e) {
-                return oDate;
             }
         }
 
